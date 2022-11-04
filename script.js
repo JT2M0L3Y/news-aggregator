d78c03e3e62ceefd04dbbf9ea50aa207dc930297
Jonathan <jsmoley@zagmail.gonzaga.edu>
2022-11-03 19:06:53 -0700

    state: structure/styling near complete

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ window.addEventListener("DOMContentLoaded", domLoaded);
 var article_count = 15;
 
 function domLoaded() {
+    var item_buttons = document.getElementsByClassName("item-button");
+
+    for (var i = 0; i < item_buttons.length; i++) {
+        item_buttons[i].addEventListener("click", itemButtonClicked);
+    }
+
     for (var i = 0; i < article_count; i++) {
         var article = document.createElement("article");
+        article.className = "news-articles";
         article.innerHTML = "<h1>Article " + i + "</h1>";
-        document.getElementsByClassName("news-content").appendChild(article);
+        document.getElementsByClassName("scroll").appendChild(article);
     }
-};
+}
+
+function itemButtonClicked() {
+    var item_buttons = document.getElementsByClassName("item-button");
+    
+    
+}
