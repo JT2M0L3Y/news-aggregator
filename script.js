5ee0b2002ea583566805d70e29d3ee169d439d0d
Jonathan <jsmoley@zagmail.gonzaga.edu>
2022-11-07 09:52:30 -0800

    api-keyed request attempted

--- script.js
+++ script.js
@@ -6,34 +6,38 @@ const publishersContainer = document.querySelector('[data-publishers]');
 const articleTemplate = document.querySelector('[data-article-template]');
 const articlesContainer = document.querySelector('[data-articles-container]');
 
-fetch("https://jsonplaceholder.typicode.com/users")
+const url = 'https://newsdata.io/api/1/news';
+const key = 'pub_12841145b63adb79f8fb5cc26a075a97222a1'
+
+fetch(url + '?apikey=' + key)
     .then(res => res.json())
     .then(data => {
-        articles = data.map(user => {
-            const topic = topicTemplate.content.cloneNode(true).children[0];
-            topicsContainer.appendChild(topic);
+        console.log(data);
+        // articles = data.map(user => {
+        //     const topic = topicTemplate.content.cloneNode(true).children[0];
+        //     topicsContainer.appendChild(topic);
 
-            const news_org = publisherTemplate.content.cloneNode(true).children[0];
-            publishersContainer.appendChild(news_org);
+        //     const news_org = publisherTemplate.content.cloneNode(true).children[0];
+        //     publishersContainer.appendChild(news_org);
 
-            const article = articleTemplate.content.cloneNode(true).children[0];
-            const title = article.querySelector('[data-title]');
-            const summary = article.querySelector('[data-summary]');
-            const publisher = article.querySelector('[data-publisher]');
+        //     const article = articleTemplate.content.cloneNode(true).children[0];
+        //     const title = article.querySelector('[data-title]');
+        //     const summary = article.querySelector('[data-summary]');
+        //     const publisher = article.querySelector('[data-publisher]');
 
-            title.textContent = user.name;
-            summary.textContent = user.company.catchPhrase;
-            publisher.textContent = user.company.name;
+        //     title.textContent = user.name;
+        //     summary.textContent = user.company.catchPhrase;
+        //     publisher.textContent = user.company.name;
 
-            articlesContainer.appendChild(article);
+        //     articlesContainer.appendChild(article);
 
-            return {
-                name: user.name,
-                summary: user.company.catchPhrase,
-                publisher: user.company.name,
-                element: article
-            };
-        })
+        //     return {
+        //         name: user.name,
+        //         summary: user.company.catchPhrase,
+        //         publisher: user.company.name,
+        //         element: article
+        //     };
+        // })
     });
 
 /* Upper-center search js to search for content */
@@ -43,17 +47,15 @@ let articles = [];
 
 searchInput.addEventListener('keydown', (e) => {
     if (e.keyCode == 13) {
-        const value = e.target.value;
+    const value = e.target.value;
 
-        articles.forEach(article => {
-            const isVisible = article.name.includes(value) ||
-                article.summary.includes(value) ||
-                article.publisher.includes(value);
-            
-            console.log(article.element)
-            console.log(article.element.classList)
-            article.element.classList.toggle('hide', !isVisible);
-        })
+    articles.forEach(article => {
+        const isVisible = article.name.includes(value) ||
+            article.summary.includes(value) ||
+            article.publisher.includes(value);
+
+        article.element.classList.toggle('hide', !isVisible);
+    })
     }
 });
 
@@ -85,3 +87,81 @@ function validateForm() {
         return false;
     }
 }
+
+// fetch("https://jsonplaceholder.typicode.com/users")
+//     .then(res => res.json())
+//     .then(data => {
+//         articles = data.map(user => {
+//             const topic = topicTemplate.content.cloneNode(true).children[0];
+//             topicsContainer.appendChild(topic);
+
+//             const news_org = publisherTemplate.content.cloneNode(true).children[0];
+//             publishersContainer.appendChild(news_org);
+
+//             const article = articleTemplate.content.cloneNode(true).children[0];
+//             const title = article.querySelector('[data-title]');
+//             const summary = article.querySelector('[data-summary]');
+//             const publisher = article.querySelector('[data-publisher]');
+
+//             title.textContent = user.name;
+//             summary.textContent = user.company.catchPhrase;
+//             publisher.textContent = user.company.name;
+
+//             articlesContainer.appendChild(article);
+
+//             return {
+//                 name: user.name,
+//                 summary: user.company.catchPhrase,
+//                 publisher: user.company.name,
+//                 element: article
+//             };
+//         })
+//     });
+
+// /* Upper-center search js to search for content */
+// const searchInput = document.querySelector('[data-search-input]');
+
+// let articles = [];
+
+// searchInput.addEventListener('keydown', (e) => {
+//     // if (e.keyCode == 13) {
+//         const value = e.target.value;
+
+//         articles.forEach(article => {
+//             const isVisible = article.name.includes(value) ||
+//                 article.summary.includes(value) ||
+//                 article.publisher.includes(value);
+            
+//             article.element.classList.toggle('hide', !isVisible);
+//         })
+//     // }
+// });
+
+// /* Upper-right tabs js to switch pages */
+// const tabs = document.querySelectorAll('[data-tab-target]');
+// const tabContents = document.querySelectorAll('[data-tab-content]');
+
+// tabs.forEach(tab => {
+//     tab.addEventListener('click', () => {
+//         const target = document.querySelector(tab.dataset.tabTarget);
+//         tabContents.forEach(tabContent => {
+//             tabContent.classList.add('hide');
+//         });
+//         tabs.forEach(tab => {
+//             tab.classList.add('hide');
+//         });
+//         tab.classList.remove('hide');
+//         target.classList.remove('hide');
+//     });
+// });
+
+// function validateForm() {
+//     var username = document.forms["login_form"]["username"].value;
+//     var password = document.forms["login_form"]["password"].value;
+//     if (username === "user" && password === "pass") {
+//         return true;
+//     } else {
+//         alert("Username and password must be filled out");
+//         return false;
+//     }
+// }
