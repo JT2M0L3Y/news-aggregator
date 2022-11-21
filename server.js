3eea9a3cae64b6106fc1c5275acb79a29a9b6510
Jonathan <jsmoley@zagmail.gonzaga.edu>
2022-11-21 08:56:03 -0800

    sql connection added

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const mysql = require('mysql');
+const config = require('./config');
 const express = require('express');
 const app = express();
 
@@ -8,6 +10,19 @@ app.use(express.static('public'));
 app.set("view engine", "ejs");
 
 app.listen(8080, () => { console.log('Server started on port 8080'); });
+
+const con = mysql.createConnection(config);
+
+con.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to MySQL");
+});
+
+const sql = "SELECT * FROM pet";
+con.query(sql, (err, res, fields) => {
+    if (err) throw err;
+    console.log(res);
+});
 
 app.post('views/register.ejs', (req, res) => {
     // allow visitor to add account in database
